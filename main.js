7f875c36aa428e224c42f4041602009ff09da0ac
giogiosgr <giovannisgr88@gmail.com>
2024-05-15 13:45:52 +0200

    fix: rivisti i commenti

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 
 /* 
 LOGICA:
-Un ciclo for che per ogni iterazione confonta il numero attuale con il risultato dell'operatore "modulo"
-rispettivamente per entrambe le cifre 3 e 5 (operatore logico AND), poi con 3 e con 5 prese singolarmente, per valutare
-in quale caso ci troviamo e quindi cosa stampiamo in console. 
+Sappiamo che discriminiamo se un numero è multiplo di un altro confrontando il resto tra i due numeri
+(se il resto è 0, abbiamo un multiplo), usando l'operatore "modulo".
+Quindi creaiamo un ciclo for che per ogni iterazione fa il confronto tra la cifra zero ed il risultato tra il numero
+dell'iterazione attuale confronto (tramite modulo) rispettivamente con i casi delle cifre 3 e 5 
+(con l'uso dell'operatore logico AND), poi con 3 e con 5 prese singolarmente, per valutare in quale caso ci 
+troviamo e quindi cosa stampiamo in console. 
 Se nessun confronto restituisce un numero multiplo né di 3 né di 5, allora stampiamo il numero stesso.
 */
 
